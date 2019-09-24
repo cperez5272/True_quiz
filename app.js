ef3bca198863ae37d7cfbcf8f8b3803c2d7b48ae
Cesar Perez <Cesar.Perez5272@yahoo.com>
2019-09-24 10:53:02 -0400

    'working on getting to page 3'

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const STORE = [
             `Speedwagon`,
             `Hol Horse`,
         ],
-        correctanswer: `Caesar Antonio Zeppeli`
+        correctAnswer: `Caesar Antonio Zeppeli`
     },
     {
         question: `Which of these characters have the longest dialog ever seen in the series?`,
@@ -18,7 +18,7 @@ const STORE = [
             `Notorious B.I.G`,
             `Yoshikage Kira`,
         ],
-        correctanswer: `Yoshikage Kira`
+        correctAnswer: `Yoshikage Kira`
     },
     {
         question: `Which Jojo apparently seems to somehow know what you will say next?`,
@@ -28,7 +28,7 @@ const STORE = [
             `Johnantan Joestar`,
             `Josuke Higashikata`,
         ],
-        correctanswer: `Joseph Joestar`
+        correctAnswer: `Joseph Joestar`
     },
     {
         question: `Which of the following stands has an insane ability that no reader or viewer could ever hope to understand?`,
@@ -38,7 +38,7 @@ const STORE = [
             `Gold Experience Requiem`, 
             `Dirty Deeds Done Dirt Cheap`,
         ],
-        correctanswer: `Dirty Deeds Done Dirt Cheap`
+        correctAnswer: `Dirty Deeds Done Dirt Cheap`
     },
     {
         question: `What was the name of the group that Bruno Bucciarat was the leader of?`,
@@ -48,7 +48,7 @@ const STORE = [
             `Speedwagon Foundation`,
             `Gangstars`, 
         ],
-        correctanswer: `Passione`
+        correctAnswer: `Passione`
     },
     {
         question: `What was the importance of the stand Rolling Stone?`,
@@ -58,17 +58,17 @@ const STORE = [
             `Learn to move on despite the struggle`,
             `Understand the importance of friends`,
         ],
-        correctanswer: `There is no way one can escape or change fate`
+        correctAnswer: `There is no way one can escape or change fate`
     },
     {
-        question: `In addition to Kira’s stand, Killer Queen, which other stand or stands Kira used against Josuke and his stand Crazy Diamond?`,
+        question: `In addition to Kira stand, Killer Queen, which other stand or stands Kira used against Josuke and his stand Crazy Diamond?`,
         answers: [
             `Stray Cat and Bad Company`,
             `Atom Heart Father and Red Hot Chili Pepper`,
             `Stray Cat and Atom Heart Father`,
             `Cinderella and Snow White`,
         ],
-        correctanswer: `Stray Cat and Atom Heart Father`
+        correctAnswer: `Stray Cat and Atom Heart Father`
     },
     {
         question: `Lisa Lisa is considered the strongest harmon user during Battle Tendency, her weapon of choice was a scarf, but how was it made?`,
@@ -78,7 +78,7 @@ const STORE = [
             `Sunflower Petals`,
             `Sunny D`,
         ],
-        correctanswer: `A Satiporoja Beetle`
+        correctAnswer: `A Satiporoja Beetle`
     },
     {
         question: `What was the name of the town where Diamond is Unbreakable took place?`,
@@ -88,7 +88,7 @@ const STORE = [
             `Egypt`,
             `Tokyo`,
         ],
-        correctanswer: `Morioh`
+        correctAnswer: `Morioh`
     },
     {
         question: `Out of the list below, what is considered the most popular jojo meme?`,
@@ -98,7 +98,7 @@ const STORE = [
             `AYYAYAYAYYYAAAA`,
             `OH MY GOD!`,
         ],
-        correctanswer: `To be continued`
+        correctAnswer: `To be continued`
     }
 ];
 // Everything above here is good!
@@ -109,39 +109,72 @@ let score = 0;
 let questionNumber = 0;
 
 function startQuiz () {
- $('.quiz_container').hide()
- $('button').click('submit', function (event) {
-    console.log('button working');
-    event.preventDefault();
-    $('header').fadeOut('fast');
-    renderQuestion();
- }
-    )}
+    $('.quiz_container').hide()
+    $('.scoreboard').hide()
+    $('.start_quiz').click('submit', function (event) {
+        console.log('button working');
+        $('header').fadeOut('fast');
+        renderQuestion();
+        $('.submit_button').click(function() {
+        // $('.quiz_container').fadeOut('fast')
+            nextQuestion()
+        })
+    })
+}
 
 function renderQuestion () {
-    $('.start_quiz').click(function() {
-        $('.quiz_container').toggle('slow', function () {
+        $('.scoreboard').show()
+        $('.quiz_container').show('slow', function () {
             $('.quiz_display').text(STORE[questionNumber].question)
             $('.answer_1').text(STORE[questionNumber].answers[0])
             $('.answer_2').text(STORE[questionNumber].answers[1])
             $('.answer_3').text(STORE[questionNumber].answers[2])
             $('.answer_4').text(STORE[questionNumber].answers[3])
-    $('.submit_button').click(function() {
-        // $('.quiz_container').fadeOut('fast')
-        nextQuestion()
-    })
+        $(".answer_form").submit(function(event) {
+            event.preventDefault()
+            $('.answer').prop('checked', false)
         })
+    })
+}
+
+function wrongAnswer () {
+    $('.response').html(`
+    <h3>Your answer was wrong!</h3>
+    <img src="https://thumbs.gfycat.com/ClearQualifiedAfricanparadiseflycatcher-size_restricted.gif" alt= "Old man screaming in fear" class= "images">
+    <p>The correct answer is actually:</p>
+    <p>${STORE[questionNumber].correctAnswer}</p>
+    <button>Continue</button>`
+    )
+}
+
+function rightAnswer () {
+    $('.response').html(`
+    <h3>Your answer was right!</h3>
+    <img src="https://miro.medium.com/max/2560/1*U291PytRexmkC81_cqArNg.jpeg" alt= "Two guys posing!" class= "images">
+    <button>Continue</button>`
+    )
+    updateScore() // work on that in a bit, but it should be called here.
+}
+
+function submitAnswer () {
+    $('.submit_button').on('click', function (event) {
+        if (STORE[questionNumber].answers === STORE[questionNumber].correctAnswer) {
+            console.log('yea yea yea')
+            rightAnswer()
+        } else {
+            console.log('nah nah nah')
+            wrongAnswer()
+        }
     })
 }
 
 function nextQuestion () {
     questionNumber++
+    renderQuestion()
 }
-
 
 //Where all the functions should be called
 function makeQuiz () {
     startQuiz();
-    renderQuestion();
 }
 $(makeQuiz)
